edffdec33bc48c3fc0b1cebaf733d86330943bf3
sa12345koko123 <kokphoto123@gmail.com>
2023-01-20 04:02:19 +0000

    練習

--- main.js
+++ main.js
@@ -36,6 +36,105 @@ let camera_y = 0;
 //星の実体
 let star=[];
 
+// キーボードの状態
+let key = [];
+
+// キーボードが押された時
+document.onkeydown = function(e){
+	key[e.keyCode] = true;
+}
+　// キーボードが話された時
+document.onkeyup = function(e){
+	key[e.keyCode] = false;
+}
+
+// 自機のクラス
+class Jiki {
+	constructor(){
+		this.y = (FIELD_W/2)<<8;
+		this.x = (FIELD_H/2)<<8;
+		this.speed = 512;
+		this.anime = 0;
+	}
+	update()
+	{
+		if( key[37] && this.x>this.speed )
+		{
+			this.x-=this.speed;
+			if(this.anime>-8 )this.anime--;
+		}
+		else if( key[39] && this.x<= (FIELD_W<<8)-this.speed )
+		{
+			this.x+=this.speed;
+			if(this.anime<8  )this.anime++;
+		}
+		else
+		{
+			if(this.anime>0) this.anime--;
+			if(this.anime<0) this.anime++;
+		}
+		
+		if( key[38] && this.y>this.speed )
+			this.y-=this.speed;
+		
+		if( key[40] && this.y<= (FIELD_H<<8)-this.speed)
+			this.y+=this.speed;
+	}
+	
+	//描画
+	draw()
+	{
+		drawSprite(2 + (this.anime>>2), this.x, this.y );
+	}
+}
+
+let jiki = new Jiki();
+
+
+// ファイル読み込み
+let spriteImage = new Image();
+spriteImage.src = "sprite (1).png";
+
+// スプライトクラス
+class Sprite {
+	constructor(x,y,w,h){
+		this.x = x;
+		this.y = y;
+		this.w =w;
+		this.h = h;
+	}
+}
+
+//スプライト
+let sprite = [
+	new Sprite(  0, 0,22,42 ),
+	new Sprite( 23, 0,33,42 ),
+	new Sprite( 57, 0,43,42 ),
+	new Sprite(101, 0,33,42 ),
+	new Sprite(135, 0,21,42 ),
+];
+
+
+// スプライトを描画する関数
+function drawSprite( snum, x, y){
+	let sx = sprite[snum].x
+	let sy = sprite[snum].y
+	let sw = sprite[snum].w
+	let sh = sprite[snum].h
+	
+	let px = (x>>8) - sw/2;
+	let py = (y>>8) - sh/2;
+	
+	if( px+sw/2 <camera_x || px-sw/2 >=camera_x+SCREEN_W 
+			|| py+sh/2 <camera_y || py-sh/2 >=camera_y+SCREEN_H )return;
+	
+	vcon.drawImage( spriteImage,sx,sy,sw,sh,px,py,sw,sh);
+}
+
+
+
+
+
 //整数のランダムを作る
 function rand(min,max){
 	return Math.floor( Math.random()*(max-min+1) )+min;
@@ -85,13 +184,23 @@ function gameLoop(){
 	//移動の処理
 	
 	for(let i=0;i<STAR_MAX;i++)star[i].update();
+	jiki.update();
 	
 	//描画の処理
-	
 	vcon.fillStyle="black";
 	vcon.fillRect(0,0,SCREEN_W,SCREEN_H);
 	
 	for(let i=0;i<STAR_MAX;i++)star[i].draw();
+	
+	jiki.draw ();
+	
+	// 自機の範囲 0 ～ FIELD_W
+	// カメラの範囲 0 ～ (FIELD_W-SCREEN_W)
+	
+	camera_x = (jiki.x>>8)/FIELD_W * (FIELD_W-SCREEN_W);
+	camera_y = (jiki.y>>8)/FIELD_H * (FIELD_H-SCREEN_H);
+	
+
 	
 	//仮想画面から実際のキャンバスにコピー
 	
@@ -103,3 +212,4 @@ function gameLoop(){
 window.onload=function(){
 	gameInit();
 }
+
